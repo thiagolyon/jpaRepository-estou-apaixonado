69f7107228b1ea012bdcd4c2998ca7382acade90
Thiago Lyon <thiago.lyon.passos@gmail.com>
2022-05-13 23:12:03 -0300

    JpaRepository. Estou apaixonado!

---
+++ jparepository/src/test/java/com/lyon/jparepository/JparepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyon.jparepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JparepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
